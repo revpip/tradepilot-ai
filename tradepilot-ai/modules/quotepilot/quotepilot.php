48ed025271879dd72b4504f4ecd5ba23b620508d
revpip <prm@russellgrant.com>
2026-06-28 17:35:04 +0100

    Add QuotePilot module placeholder v0.3.0

---
+++ tradepilot-ai/modules/quotepilot/quotepilot.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * TradePilot AI
+ * Module: QuotePilot
+ * Function: Quote and estimate workflow module placeholder.
+ * Version: 0.1.0
+ *
+ * @package TradePilotAI
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
