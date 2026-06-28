c87f6329d472c428f0c5bd7df95fec1df3cc098c
revpip <prm@russellgrant.com>
2026-06-28 17:35:11 +0100

    Add RoutePilot module placeholder v0.3.0

---
+++ tradepilot-ai/modules/routepilot/routepilot.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * TradePilot AI
+ * Module: RoutePilot
+ * Function: Contractor routing and territory module placeholder.
+ * Version: 0.1.0
+ *
+ * @package TradePilotAI
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
