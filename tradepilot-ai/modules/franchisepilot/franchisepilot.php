be90283f5a5ff195bb971dc0ac94e1dd98778be3
revpip <prm@russellgrant.com>
2026-06-28 17:35:43 +0100

    Add FranchisePilot module placeholder v0.3.0

---
+++ tradepilot-ai/modules/franchisepilot/franchisepilot.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * TradePilot AI
+ * Module: FranchisePilot
+ * Function: Franchise, territory and SaaS licensing module placeholder.
+ * Version: 0.1.0
+ *
+ * @package TradePilotAI
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
